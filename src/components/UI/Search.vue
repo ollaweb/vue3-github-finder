d45e1c9de9995dcca7b05d329e0a10ef9bac94d1
Olga L <olla.bradford@gmail.com>
2022-05-25 17:13:18 +0300

    Added request to GitHub API. Showing repos

---
+++ src/components/UI/Search.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="wrapper__search">
+    <!-- Search -->
+    <div class="search">
+      <div class="search-icon">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-search"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+        </svg>
+      </div>
+    </div>
+    <!-- Search input -->
+    <input type="text" :placeholder="placeholder" v-model="search" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    placeholder: {
+      type: String,
+      default: 'Search'
+    }
+  },
+  data() {
+    return { search: this.value }
+  },
+  watch: {
+    search(val) {
+      // console.log(val)
+      this.$emit('search', val)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+/* Search */
+.wrapper__search {
+  margin: 50px 0 30px;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  & svg {
+    color: #e4e4e4;
+  }
+  & input {
+    margin: 0 auto;
+    padding: 16px 16px 16px 56px !important;
+    margin-bottom: 0px;
+    border-radius: 30px;
+    color: #909399;
+    font-size: 16px;
+
+    height: auto;
+  }
+}
+.search {
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  position: absolute;
+}
+.search-icon {
+  height: 100%;
+  padding-left: 16px;
+  padding-right: 16px;
+  align-items: center;
+  display: flex;
+}
+
+.notes-show {
+  text-align: right;
+  flex: 0 1 20%;
+  & svg {
+    color: #999;
+    cursor: pointer;
+    &.active {
+      color: #0e39c9;
+    }
+    &:not(:last-child) {
+      margin-right: 10px;
+    }
+  }
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,89 @@
 <template>
-    <h1>Home page</h1>
+  <form @submit.prevent="getRepos" class="search-block">
+    <Search
+      @search="search = $event"
+      :value="search"
+      placeholder="Type user name..."
+    />
+    <button class="btn btnPrimary btn-search" type="submit">Search</button>
+  </form>
+
+  <ul class="repo" v-if="repos">
+    <h2 class="repo-title">Repos:</h2>
+    <li v-for="repo in repos" :key="repo.id" class="repo__item">
+      <div class="repo__info">
+        <a class="link" target="_blank" :href="repo.html_url"
+          >{{ repo.name }}
+        </a>
+
+        <span>{{ repo.stargazers_count }} ⭐</span>
+      </div>
+    </li>
+  </ul>
 </template>
+
+<script>
+import Search from '@/components/UI/Search.vue'
+
+export default {
+  components: {
+    Search
+  },
+  data() {
+    return {
+      search: '',
+      repos: null
+    }
+  },
+  methods: {
+    async getRepos() {
+      // console.log(`get user ${this.search} repos`)
+      await fetch(`https://api.github.com/users/${this.search}/repos`)
+        .then(response => {
+          // console.log(response.json())
+          return response.json()
+        })
+        .then(data => {
+          this.repos = data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.search-block {
+  margin: 0 auto;
+  width: 100%;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+}
+.btn-search {
+  margin: 0 auto;
+  width: 50%;
+}
+.repo-title {
+  margin: 20px auto 10px;
+  width: 100%;
+  max-width: 500px;
+  font-weight: 700;
+}
+.repo {
+  margin: 0px auto 30px;
+  width: 100%;
+  max-width: 500px;
+  &__item {
+    padding: 3px 0;
+  }
+  &__info {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #dbdbdb;
+  }
+}
+</style>
