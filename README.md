b50226e68cb91fdf4f436d9dffc16a8fd5231bdd
Olga L <olla.bradford@gmail.com>
2022-06-05 19:38:15 +0300

    Added license and updated Readme file

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-# GitHub Finder
+# SPA - GitHub Finder (Vue 3)
+
+[Demo to test my project](https://ollaweb.github.io/vue3-github-finder/)
+
+## Review
+
+![Look at this lovely design](https://github.com/ollaweb/vue3-github-finder/blob/main/GitHub-Finder.JPG)
+
+## Demonstration
+
+![Demonstration](https://github.com/ollaweb/vue3-github-finder/blob/main/demonstration.gif)
+
+### Description
+
+Study project. Getting data by REST API from GitHub API.
+Project made by learning Vue 3 [in this course.](https://tocode.ru/courses/vuejs-3-s-nulya-do-rezultata/)
+
+#### In this project:
+
+- using options API;
+- getting data with REST API;
+- showing data;
+- sorting;
+- pagination (loading more).
 
 ## Project setup
 
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,14 @@
     <footer class="footer">
         <div class="container">
         <ul class="navbar-list">
-            <li class="navbar-item" v-for="link in links" :key="link.alias">
-            <router-link class="navbar-link" :to="link.url">{{link.title}}</router-link>
-            </li>
+          <li class="navbar-item" v-for="link in links" :key="link.alias">
+          <router-link class="navbar-link" :to="link.url">{{link.title}}</router-link>
+          </li>
+          <li>
+            <a class="navbar-link" target="_blank" href="https://github.com/ollaweb/vue3-github-finder">visit my repo
+              <img src="../assets/img/github.svg" alt="github">
+            </a>
+          </li>
         </ul>
 
         <!-- link to course -->
